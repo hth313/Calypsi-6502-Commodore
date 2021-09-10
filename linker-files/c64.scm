39ea6cfb995d73ddb568c25a727359d81ec346bb
Håkan Thörngren <hth313@gmail.com>
2021-09-09 21:22:36 -0700

    Revise the C64 linker rules file
    Add missing zero page sections and reduce the default size of stack
    and heap.

--- linker-files/c64.scm
+++ linker-files/c64.scm
@@ -2,17 +2,17 @@
   '((memory program
             (address (#x801 . #xbfff))
             (section (programStart #x801) (startup #x80e)
-                     code switch data cdata data_init_table))
+                     code switch data cdata data_init_table izpage idata))
     (memory zeroPage
             (address (#x2 . #xff))
-            (section registers))
+            (section registers zzpage zpage))
     (memory stack
             (address (#x100 . #x1ff))
             (section stack))
     (memory data
             (address (#xc000 . #xcfff))
             (section cstack data zdata heap))
-    (block cstack (size #x800))               ; C stack size
+    (block cstack (size #x400))               ; C stack size
     (block stack  (size #x100))               ; machine stack size
-    (block heap   (size #x800))               ; heap size
+    (block heap   (size #x200))               ; heap size
     ))
