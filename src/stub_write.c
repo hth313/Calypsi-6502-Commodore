b4bcf4fb9dc16e1614618f5531b763d4ceaf9f1f
Håkan Thörngren <hth313@gmail.com>
2021-09-11 17:56:35 -0700

    Do not use ssize_t
    It does not exist, it is a POSIX thing that is not supported.

--- src/stub_write.c
+++ src/stub_write.c
@@ -3,9 +3,9 @@
 
 #define CHROUT(c) ( (void (*)(char) ) 0xffd2) (c)
 
-ssize_t _Stub_write (int fd, const void *buf, size_t count) {
+size_t _Stub_write (int fd, const void *buf, size_t count) {
   const char *p = buf;
-  ssize_t n = 0;
+  size_t n = 0;
 
   switch (fd) {
   case 1: {
@@ -20,7 +20,7 @@ ssize_t _Stub_write (int fd, const void *buf, size_t count) {
     }
   default:
     __set_errno(EBADF);
-    return (ssize_t) -1;
+    return (size_t) -1;
   }
   return n;
 }
