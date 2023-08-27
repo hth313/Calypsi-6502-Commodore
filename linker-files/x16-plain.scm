c4ac1f0f023df0fe0afbc0f4ec0609fe77910ba3
Håkan Thörngren <hth313@gmail.com>
2023-08-27 08:51:26 -0700

    Add X16 linker rules

---
+++ linker-files/x16-plain.scm
@@ -0,0 +1,7 @@
+(define memories
+  '((memory program
+            (address (#x801 . #x9fff)) (type any)
+            (section (programStart #x801) (startup #x80e)))
+    (memory zeroPage (address (#x2 . #x7f)) (type ram) (qualifier zpage)
+	    (section (registers #x2)))
+    ))
