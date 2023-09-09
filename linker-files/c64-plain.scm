11c76418884f887f5b25deb34fb11b84960bf0ac
Håkan Thörngren <hth313@gmail.com>
2023-09-08 18:57:14 -0700

    Reduce C64 area not to cover BASIC ROM

--- linker-files/c64-plain.scm
+++ linker-files/c64-plain.scm
@@ -1,6 +1,6 @@
 (define memories
   '((memory program
-            (address (#x801 . #xbfff)) (type any)
+            (address (#x801 . #x9fff)) (type any)
             (section (programStart #x801) (startup #x80e)))
     (memory zeroPage (address (#x2 . #xff)) (type ram) (qualifier zpage))
     (memory upperdata
