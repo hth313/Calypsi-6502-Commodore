7e645ef0c2d157133f138559d33cb9ad4e8d1949
Håkan Thörngren <hth313@gmail.com>
2024-02-04 22:32:56 -0800

    Add a banked .scm for MEGA65
    Provide two, one plain for non-banked applications and one banked
    which should be good starting points.

--- linker-files/mega65-banked.scm
+++ linker-files/mega65-banked.scm
@@ -5,4 +5,6 @@
     (memory zeroPage (address (#x2 . #x7f)) (type ram) (qualifier zpage)
 	    (section (registers #x2)))
     (memory stackPage (address (#x100 . #x1ff)) (type ram))
+    (memory bank1  (address (#x10000 . #x1ffff)) (type data) (qualifier far))
+    (memory bank45 (address (#x40000 . #x5ffff)) (type data) (qualifier far))
     ))
