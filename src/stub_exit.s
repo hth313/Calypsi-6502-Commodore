5a8bb04019a90c471c624fe59d0d6e2eaa5ab938
Håkan Thörngren <hth313@gmail.com>
2024-02-04 22:29:01 -0800

    Minor correction to _Stub_exit()
    Made it weak and added the cstartup runtime attribute to it. This
    will pull it in when the Commodore style cstartup is used, which makes
    some sense as they both use the _InitialStack variable for keeping
    track of the stack when exiting back.

--- src/stub_exit.s
+++ src/stub_exit.s
@@ -1,5 +1,15 @@
-              .section code
-              .public _Stub_exit
+#if defined(__CALYPSI_TARGET_SYSTEM_C64__)
+              .rtmodel cstartup,"c64"
+#elif defined(__CALYPSI_TARGET_SYSTEM_MEGA65__)
+              .rtmodel cstartup,"mega65"
+#elif defined(__CALYPSI_TARGET_SYSTEM_CX16__)
+              .rtmodel cstartup,"cx16"
+#else
+#pragma GCC error "--target system not one of C64, MEGA65 or X16"
+#endif
+
+	      .section code
+              .pubweak _Stub_exit
               .extern _Zp, _InitialStack
 _Stub_exit:   ldx     _InitialStack
               txs
