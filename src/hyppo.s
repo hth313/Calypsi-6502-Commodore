84b3e9f676662f65bf5b8dea3c78b98459fa1eef
Håkan Thörngren <hth313@gmail.com>
2024-10-01 20:39:32 -0700

    MEGA65: Add Hyppo trap address declarations

---
+++ src/hyppo.s
@@ -0,0 +1,85 @@
+#if defined(__CALYPSI_TARGET_SYSTEM_MEGA65__)
+
+	      .public HTRAP00, HTRAP01, HTRAP02, HTRAP03, HTRAP04
+	      .public HTRAP05, HTRAP06, HTRAP07, HTRAP08, HTRAP09
+	      .public HTRAP0A, HTRAP0B, HTRAP0C, HTRAP0D, HTRAP0E
+	      .public HTRAP0F
+	      .public HTRAP10, HTRAP11, HTRAP12, HTRAP13, HTRAP14
+	      .public HTRAP15, HTRAP16, HTRAP17, HTRAP18, HTRAP19
+	      .public HTRAP1A, HTRAP1B, HTRAP1C, HTRAP1D, HTRAP1E
+	      .public HTRAP1F
+	      .public HTRAP20, HTRAP21, HTRAP22, HTRAP23, HTRAP24
+	      .public HTRAP25, HTRAP26, HTRAP27, HTRAP28, HTRAP29
+	      .public HTRAP2A, HTRAP2B, HTRAP2C, HTRAP2D, HTRAP2E
+	      .public HTRAP2F
+	      .public HTRAP30, HTRAP31, HTRAP32, HTRAP33, HTRAP34
+	      .public HTRAP35, HTRAP36, HTRAP37, HTRAP38, HTRAP39
+	      .public HTRAP3A, HTRAP3B, HTRAP3C, HTRAP3D, HTRAP3E
+	      .public HTRAP3F
+
+HTRAP00:      .equlab 0xd640
+HTRAP01:      .equlab 0xd641
+HTRAP02:      .equlab 0xd642
+HTRAP03:      .equlab 0xd643
+HTRAP04:      .equlab 0xd644
+HTRAP05:      .equlab 0xd645
+HTRAP06:      .equlab 0xd646
+HTRAP07:      .equlab 0xd647
+HTRAP08:      .equlab 0xd648
+HTRAP09:      .equlab 0xd649
+HTRAP0A:      .equlab 0xd64a
+HTRAP0B:      .equlab 0xd64b
+HTRAP0C:      .equlab 0xd64c
+HTRAP0D:      .equlab 0xd64d
+HTRAP0E:      .equlab 0xd64e
+HTRAP0F:      .equlab 0xd64f
+HTRAP10:      .equlab 0xd650
+HTRAP11:      .equlab 0xd651
+HTRAP12:      .equlab 0xd652
+HTRAP13:      .equlab 0xd653
+HTRAP14:      .equlab 0xd654
+HTRAP15:      .equlab 0xd655
+HTRAP16:      .equlab 0xd656
+HTRAP17:      .equlab 0xd657
+HTRAP18:      .equlab 0xd658
+HTRAP19:      .equlab 0xd659
+HTRAP1A:      .equlab 0xd65a
+HTRAP1B:      .equlab 0xd65b
+HTRAP1C:      .equlab 0xd65c
+HTRAP1D:      .equlab 0xd65d
+HTRAP1E:      .equlab 0xd65e
+HTRAP1F:      .equlab 0xd65f
+HTRAP20:      .equlab 0xd660
+HTRAP21:      .equlab 0xd661
+HTRAP22:      .equlab 0xd662
+HTRAP23:      .equlab 0xd663
+HTRAP24:      .equlab 0xd664
+HTRAP25:      .equlab 0xd665
+HTRAP26:      .equlab 0xd666
+HTRAP27:      .equlab 0xd667
+HTRAP28:      .equlab 0xd668
+HTRAP29:      .equlab 0xd669
+HTRAP2A:      .equlab 0xd66a
+HTRAP2B:      .equlab 0xd66b
+HTRAP2C:      .equlab 0xd66c
+HTRAP2D:      .equlab 0xd66d
+HTRAP2E:      .equlab 0xd66e
+HTRAP2F:      .equlab 0xd66f
+HTRAP30:      .equlab 0xd670
+HTRAP31:      .equlab 0xd671
+HTRAP32:      .equlab 0xd672
+HTRAP33:      .equlab 0xd673
+HTRAP34:      .equlab 0xd674
+HTRAP35:      .equlab 0xd675
+HTRAP36:      .equlab 0xd676
+HTRAP37:      .equlab 0xd677
+HTRAP38:      .equlab 0xd678
+HTRAP39:      .equlab 0xd679
+HTRAP3A:      .equlab 0xd67a
+HTRAP3B:      .equlab 0xd67b
+HTRAP3C:      .equlab 0xd67c
+HTRAP3D:      .equlab 0xd67d
+HTRAP3E:      .equlab 0xd67e
+HTRAP3F:      .equlab 0xd67f
+
+#endif
